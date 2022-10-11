b2ecf4ae93a10ca1eb4a097233d183c47b6547ef
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-10-11 15:17:40 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+// Seleção de elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+const slides = document.querySelectorAll(".banner");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
+
+// Funções
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+
+  setTimeout(() => {
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+  }, 500);
+}
+
+function showSlides() {
+  console.log(slides);
+  let i;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slideIndex - 1].classList.add("active");
+
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 3000);
+}
+
+// Eventos
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active");
+  });
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
+});
+
+// Inicialização
+showSlides();
